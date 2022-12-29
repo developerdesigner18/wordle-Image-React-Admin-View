9221507c60cda8b53a21d3833fbcd205125e418c
developerdesigner18 <developer.designer18@gmail.com>
2022-12-29 17:33:05 +0530

    Add: LoginRestrictedRoute

--- admin-view/src/App.js
+++ admin-view/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Adminlogin from './component/Adminlogin/Adminlogin';
 import ForgetPassword from "./component/Adminlogin/ForgetPassword/ForgetPassword";
 import ProtectedRoute from './component/ProtectedRoute/ProtectedRoute';
+import LoginRestrictedRoute from './component/LoginRestrictedRoute/LoginRestrictedRoute'
 import Layout from './component/Layout/Layout';
 import Dashboard from './component/Dashboard/Dashboard';
 import Challenges from './component/Challenges/Challenges';
@@ -12,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Adminlogin/>}></Route>
+      <Route path="/" element={<LoginRestrictedRoute><Adminlogin/></LoginRestrictedRoute>}></Route>
       <Route path="/forgetpassword" element={<ForgetPassword/>}></Route>
       <Route path="/dashboard" element={<ProtectedRoute><Layout><Dashboard /></Layout></ProtectedRoute>}></Route>
       <Route path="/challenges" element={<ProtectedRoute><Layout><Challenges /></Layout></ProtectedRoute>}></Route>
---
+++ admin-view/src/component/LoginRestrictedRoute/LoginRestrictedRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+// THIS PROTECTED ROUTE CHECKS THAT ID LOGGED IN USER IS ADMIN THEN ONLY ALOWED TO USER ADMIN PANEL
+function LoginRestrictedRoute({ children }) {
+  if (
+    localStorage.getItem("token") &&
+    JSON.parse(localStorage.getItem("user"))?.username === "admin" &&
+    JSON.parse(localStorage.getItem("user"))?.role === "admin"
+  ) {
+    return <Navigate to="/dashboard"></Navigate>;
+  } else {
+    // ELSE REDIRECT TO THE LOGIN PAGE AUTOMATICALLY
+    return <>{children}</>;
+  }
+}
+
+export default LoginRestrictedRoute;
